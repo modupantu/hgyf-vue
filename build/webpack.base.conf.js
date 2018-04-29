3126f2b0ae760ebb2e7c9793ca3a74b5ec19e8d1
魔都叛徒 <caoruichun@yeah.net>
2018-04-29 18:28:45 +0800

    Add files via upload

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,17 +3,13 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
-
-
-
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill','./src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
